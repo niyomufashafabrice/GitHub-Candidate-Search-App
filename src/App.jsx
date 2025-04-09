91c9a075e946d7bf52908252648dd3868580ca46
niyomufashafabrice <fabrice1niyo@gmail.com>
2025-04-09 11:41:59 +0200

    using teilwind to make sytem look well

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { CiSearch } from "react-icons/ci";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const App = () => {
 
@@ -21,11 +22,12 @@ const App = () => {
   };
 
   return (
-    <div className="flex justify-center item-center px=[40] h-[600px]">
-      <div className="App container  justify-center">
-      <h1 className=' tetx-4xl font-bold py-4'>GitHub Candidate Search</h1>
+    <div className="flex justify-center item-center py-[50px] px-[40] h-[100vh] bg-[#070942]">
+      <div className="App container   items-center flex flex-col gap-5">
+      <h1 className=' text-6xl font-bold py-4 text-white'>GitHub Candidate Search</h1>
+      <div className='bg-white flex justify-end items-center w-[28%] rounded-2xl'>
       <input
-        className=' border border-amber-200 mx-[30px] p-4 rounded-2xl'
+        className='  mx-[30px] p-4 rounded-2xl focus:outline-none focus:border-none'
         type="text"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
@@ -33,17 +35,20 @@ const App = () => {
       />
       <button onClick={handleSearch}
        className='border border-amber-200 px-[30px] py-[12px] rounded-2xl hover:scale-[1.1] hover:bg-amber-200 hover:text-black cursor-pointer'>Search</button>
+      </div>
 
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
       {userData && (
-        <div className="profile mt-[40px]">
+        <div className="profile mt-[40px] bg-white shadow-lg p-6 bg-gradient-to-b from-[#bebad4] to-[#41358aec] w-[60%]">
           <img src={userData.avatar_url} alt="Avatar" width="100" className='rounded-full my-2' />
-          <h2 className='text-xl '>{userData.name || 'No Name Provided'}</h2>
-          <p>{userData.bio || 'No bio'}</p>
-          <p>Public Repos: {userData.public_repos}</p>
-          <p>Followers: {userData.followers}</p>
-          <a href={userData.html_url} target="_blank" rel="noreferrer">View GitHub Profile</a>
+          <h2 className='text-2xl font-bold'>{userData.name || 'No Name Provided'}</h2>
+          <p className='text-lg  font-semibold italic'>{userData.bio || 'No bio'}</p>
+          <div className="border-t border-gray-300 my-4"></div>
+
+          <p className='text-lg font-semibold'>Public Repos: {userData.public_repos}</p>
+          <p className='text-lg font-semibold'>Followers: {userData.followers}</p>
+          <a href={userData.html_url} target="_blank" rel="noreferrer" className='text-lg text-amber-200 flex  flex-row items-center italic gap-2'>View GitHub Profile <FaExternalLinkAlt /></a>
         </div>
       )}
     </div>
